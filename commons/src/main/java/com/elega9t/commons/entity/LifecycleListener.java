52ff935e81be5c297c1853e29045457efe545169
Yogesh Dubey <AztechG0ld>
2012-09-28 12:43:55 +0100

    YD: Adding copyright to source files

--- commons/src/main/java/com/elega9t/commons/entity/LifecycleListener.java
+++ commons/src/main/java/com/elega9t/commons/entity/LifecycleListener.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2011 - 2012. Elega9t Ltd. All rights reserved.
+ * ELEGA9T PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.Copyright (c) 2011 - 2012. Elega9t Ltd. All rights reserved.
+ */
+
 package com.elega9t.commons.entity;
 
 public interface LifecycleListener {
