9c38e6b9e3faa927218c4222458436ab4b5a6bca
yogesh <dubey.yogesh@hotmail.com>
2012-11-25 20:58:31 +0000

    YD: TableDataRenderer implementation with custom border schemes

--- commons/src/main/java/com/elega9t/commons/renderer/DataProvider.java
+++ commons/src/main/java/com/elega9t/commons/renderer/DataProvider.java
@@ -1,13 +1,7 @@
 package com.elega9t.commons.renderer;
 
-/**
- * Created with IntelliJ IDEA.
- * User: yogesh
- * Date: 25/11/2012
- * Time: 18:36
- * To change this template use File | Settings | File Templates.
- */
 public interface DataProvider<T> {
+
     int rowCount();
 
     int columnCount();
@@ -15,4 +9,5 @@ public interface DataProvider<T> {
     String value(int row, int column);
 
     String columnName(int column);
+
 }
