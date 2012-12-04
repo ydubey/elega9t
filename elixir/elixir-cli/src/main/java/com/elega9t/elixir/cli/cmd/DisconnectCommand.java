79834b75f82eb597cadf7e1e34993245632014a7
Yogesh Dubey <AztechG0ld>
2012-12-04 16:26:41 +0000

    YD: Added command to close db connection

---
+++ elixir/elixir-cli/src/main/java/com/elega9t/elixir/cli/cmd/DisconnectCommand.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2011 - 2012. Elega9t Ltd. All rights reserved.
+ * ELEGA9T PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.Copyright (c) 2011 - 2012. Elega9t Ltd. All rights reserved.
+ */
+
+package com.elega9t.elixir.cli.cmd;
+
+import com.elega9t.commons.entity.DefaultEntity;
+import com.elega9t.commons.shell.Shell;
+import com.elega9t.commons.shell.intrprtr.Command;
+import com.elega9t.elixir.Connection;
+
+public class DisconnectCommand extends DefaultEntity implements Command {
+
+    public DisconnectCommand() {
+        super("disconnect");
+    }
+
+    @Override
+    public int execute(Shell shell) throws Exception {
+        Connection connection = (Connection) shell.getContextElement("connection");
+        if(connection != null) {
+            connection.close();
+            shell.outln("Connection closed.");
+        } else {
+            throw new IllegalArgumentException("No database connection exists. Please connect to a database first.");
+        }
+        return 0;
+    }
+
+}
