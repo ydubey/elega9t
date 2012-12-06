1f97d7286477c2159d002feecf0228ed29909956
Yogesh Dubey <AztechG0ld>
2012-12-06 15:24:46 +0000

    YD: refactor

--- commons/src/main/java/com/elega9t/commons/args/ParsedArgument.java
+++ commons/src/main/java/com/elega9t/commons/args/ParsedArgument.java
@@ -17,18 +17,6 @@ public class ParsedArgument {
         this.value = value;
     }
 
-    public Boolean getBooleanValue() {
-        return value != null ? Boolean.valueOf(value) : null;
-    }
-
-    public Integer getIntValue() {
-        return value != null ? Integer.valueOf(value) : null;
-    }
-
-    public Long getLongValue() {
-        return value != null ? Long.valueOf(value) : null;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
