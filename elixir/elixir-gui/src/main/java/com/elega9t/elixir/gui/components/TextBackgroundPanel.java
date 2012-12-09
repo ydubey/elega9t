09803129223788af438938cf61721953c98b43ec
yogesh <dubey.yogesh@hotmail.com>
2012-12-09 12:36:28 +0000

    YD: Added module for elixir release

--- elixir/elixir-gui/src/main/java/com/elega9t/elixir/gui/components/TextBackgroundPanel.java
+++ elixir/elixir-gui/src/main/java/com/elega9t/elixir/gui/components/TextBackgroundPanel.java
@@ -29,7 +29,6 @@ public class TextBackgroundPanel extends javax.swing.JPanel {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
-
         setLayout(new java.awt.BorderLayout());
     }// </editor-fold>//GEN-END:initComponents
     // Variables declaration - do not modify//GEN-BEGIN:variables
