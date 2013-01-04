f52accfc9ea29527dd966c2f48f0ac0a0e541d79
Yogesh Dubey <AztechG0ld>
2013-01-04 12:20:16 +0000

    YD: added Query History Dock

---
+++ elixir/elixir-gui-v2/src/main/java/com/elega9t/elixir/gui/mgrs/log/LogEvent.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2011 - 2012. Elega9t Ltd. All rights reserved.
+ * ELEGA9T PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.Copyright (c) 2011 - 2012. Elega9t Ltd. All rights reserved.
+ */
+
+package com.elega9t.elixir.gui.mgrs.log;
+
+public class LogEvent {
+
+    private LogType logType;
+    private String log;
+
+    public LogType getLogType() {
+        return logType;
+    }
+
+    public String getLog() {
+        return log;
+    }
+
+}
---
+++ elixir/elixir-gui-v2/src/main/java/com/elega9t/elixir/gui/mgrs/log/LogListener.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2011 - 2012. Elega9t Ltd. All rights reserved.
+ * ELEGA9T PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.Copyright (c) 2011 - 2012. Elega9t Ltd. All rights reserved.
+ */
+
+package com.elega9t.elixir.gui.mgrs.log;
+
+public interface LogListener {
+
+    void log(LogEvent event);
+
+}
